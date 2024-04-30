ce6fe7cc0a43278de71f332c0e1612072f188b05
Julius <juliuscecilia33@gmail.com>
2024-04-29 17:29:59 -0700

    font sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       className={`font-heading flex min-h-screen flex-row p-12 justify-between ${spaceGrotesk.className}`}
     >
       <section
-        className={`w-1/3font-heading flex min-h-screen flex-col ${spaceGrotesk.className}`}
+        className={`mr-10 w-1/5 font-heading flex min-h-screen flex-col ${spaceGrotesk.className}`}
       >
         <Image
           src={MyProfile}
@@ -21,13 +21,13 @@ export default function Home() {
           height={185}
           className="rounded-lg mb-8"
         />
-        <h1 className="text-4xl">Julius Cecilia</h1>
+        <h1 className="text-3xl">Julius Cecilia</h1>
         <hr className="border-b-2 border-gray-300 my-8 rounded-full" />
       </section>
       <section
-        className={`bg-[#2E303E] p-10 rounded-lg w-2/3 bg-gray-300 font-heading flex min-h-screen flex-col ${spaceGrotesk.className}`}
+        className={`ml-10 bg-[#2E303E] p-10 rounded-lg w-4/5 bg-gray-300 font-heading flex min-h-screen flex-col ${spaceGrotesk.className}`}
       >
-        <h1 className="text-4xl">Experience</h1>
+        <h1 className="text-3xl">Experience</h1>
       </section>
     </main>
   );
