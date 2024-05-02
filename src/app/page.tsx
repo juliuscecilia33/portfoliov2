122705f6b8e36465a8b01bc6e2ccedeefa2454df
Julius <juliuscecilia33@gmail.com>
2024-05-01 17:00:12 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       className={`font-heading flex min-h-screen flex-row p-12 justify-between ${spaceGrotesk.className}`}
     >
       <section
-        className={`mr-10 w-1/5 font-heading flex min-h-screen flex-col ${spaceGrotesk.className}`}
+        className={`pt-10 mr-10 w-1/5 font-heading flex min-h-screen flex-col ${spaceGrotesk.className}`}
       >
         <Image
           src={MyProfile}
@@ -29,7 +29,7 @@ export default function Home() {
         className={`ml-10 bg-[#2e303e] p-10 rounded-lg w-4/5 bg-gray-300 font-heading flex min-h-screen flex-col ${spaceGrotesk.className}`}
       >
         <h1 className="text-2xl font-bold mb-8">Experience</h1>
-        <div className="flex flex-col w-full p-4 bg-[#1C1E26] rounded-lg border border-[#E95278]">
+        <div className="flex flex-col w-full p-10 bg-[#1C1E26] rounded-lg border border-[#E95278]">
           <div className="flex w-full">
             <div className="aspect-square w-2/12 h-2/12 flex p-6 bg-[#E95278] rounded">
               <Image
@@ -40,10 +40,10 @@ export default function Home() {
             </div>
             <div className="w-full flex flex-col p-5 bg-[#2E303E] rounded ml-4">
               <div className="flex items-end">
-                <h2 className="text-3xl font-bold mr-2">
+                <h2 className="text-3xl font-bold mr-3">
                   Software Engineer Intern
                 </h2>
-                <p className="text-[#E95278] font-bold">Adobe</p>
+                <p className="text-[#E95278] font-bold mb-1">Adobe</p>
               </div>
             </div>
           </div>
