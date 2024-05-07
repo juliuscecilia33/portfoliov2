27efb0f87ac3cdedf2ecf29737b6680b897e1b00
Julius <juliuscecilia33@gmail.com>
2024-05-07 16:25:42 -0700

    finished reusable Experience component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 import MyProfile from "../assets/images/JuliusCecilia.jpeg";
 import Adobe from "../assets/images/Adobe.png";
 import { useState } from "react";
-import { FaChevronDown } from "react-icons/fa";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import Experience from "../components/Experience";
 
 const spaceGrotesk = Space_Grotesk({
   preload: false,
@@ -34,62 +35,26 @@ export default function Home() {
         className={`ml-10 bg-[#2E303E] p-10 rounded-lg w-4/5 font-heading flex min-h-screen flex-col ${spaceGrotesk.className}`}
       >
         <h1 className="text-2xl font-bold mb-8">Experience</h1>
-        <div className="flex flex-col w-full px-10 pt-8 pb-5 bg-[#1C1E26] rounded-lg border border-[#E95278]">
-          <div className="flex w-full">
-            <div className="aspect-square flex p-8 bg-[#E95278] rounded">
-              <Image
-                src={Adobe}
-                alt="Adobe"
-                className="aspect-square rounded-lg items-center"
-              />
-            </div>
-            <div className="w-full flex flex-col py-6 px-7 bg-[#2E303E] rounded ml-4">
-              <div className="flex items-end mb-2">
-                <h2 className="text-3xl font-bold mr-3">
-                  Software Engineer Intern
-                </h2>
-                <p className="text-[#E95278] font-bold">Adobe</p>
-              </div>
-              <h2 className="text-1xl mr-3 mb-3">June 2023 - September 2023</h2>
-              <div className="flex">
-                <div className="border border-[#569CD6] py-1 px-4 rounded-full mr-4">
-                  <h3 className="uppercase text-[#569CD6] text-xs font-bold text-center">
-                    Javascript
-                  </h3>
-                </div>
-                <div className="border border-[#569CD6] py-1 px-4 rounded-full mr-4">
-                  <h3 className="uppercase text-[#569CD6] text-xs font-bold text-center">
-                    Python
-                  </h3>
-                </div>
-                <div className="border border-[#569CD6] py-1 px-4 rounded-full mr-4">
-                  <h3 className="uppercase text-[#569CD6] text-xs font-bold text-center">
-                    Node.js
-                  </h3>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="mt-5 mx-auto flex flex-col font-bold items-center">
-            <button onClick={() => setIsOpen(!isOpen)}>
-              <h3 className="mb-1">Read More</h3>
-            </button>
-            <button onClick={() => setIsOpen(!isOpen)} className="p-0 m-0">
-              <FaChevronDown />
-            </button>
-          </div>
-          <div
-            className={`flex flex-col w-full opacity-0 transition-opacity duration-400 ${
-              isOpen ? "opacity-100 h-full" : "h-0"
-            }`}
-          >
-            <p className="text-[#E95278] font-bold mt-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-              risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing
-              nec, ultricies sed, dolor.
-            </p>
-          </div>
-        </div>
+        <Experience
+          imageSrc={Adobe}
+          imageAlt="Adobe"
+          description={[
+            "Crafted a dynamic email automation solution using Microsoft Outlookʼs Actionable Messages/Adaptive Cards feature and Googleʼs AMP feature for Gmail, optimizing product approval workflows and reducing errors by 30% for Adobeʼs business customers",
+            "Integrated the email solution to Adobe's backend services while maintaining the secure architecture, using Python and Node.js, to guarantee data security and reliability",
+            "Played a key role in the transition of Adobe's Console for Businesses from Angular.js to React.js which required using testing protocols like Jasmine.js and Jest.js to validate the integration and guarantee system performance",
+          ]}
+          positionName="Software Engineer Intern"
+          companyName="Adobe"
+          dateOfExperience="June 2023 - September 2023"
+          programmingLanguages={[
+            "Python",
+            "Node.js",
+            "Angular.js",
+            "React.js",
+            "Jasmine.js",
+            "Jest.js",
+          ]}
+        />
       </section>
     </main>
   );
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,20 +1,32 @@
 import PropTypes from "prop-types";
-import Image from "next/image";
-import { FaChevronDown } from "react-icons/fa";
+import Image, { StaticImageData } from "next/image";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { useState } from "react";
+import { Space_Grotesk } from "next/font/google";
 
 type ExperienceProps = {
-  imageSrc: string;
+  imageSrc: StaticImageData;
   imageAlt: string;
-  experiences: string[];
   description: string[];
+  positionName: string;
+  companyName: string;
+  programmingLanguages: string[];
+  dateOfExperience: string;
 };
 
 const spaceGrotesk = Space_Grotesk({
   preload: false,
 });
 
-const Experience = ({ imageSrc, imageAlt }: ExperienceProps) => {
+const Experience = ({
+  imageSrc,
+  imageAlt,
+  description,
+  positionName,
+  companyName,
+  programmingLanguages,
+  dateOfExperience,
+}: ExperienceProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -22,35 +34,28 @@ const Experience = ({ imageSrc, imageAlt }: ExperienceProps) => {
       <div className="flex w-full">
         <div className="aspect-square flex p-8 bg-[#E95278] rounded">
           <Image
-            src={Adobe}
-            alt="Adobe"
+            src={imageSrc}
+            alt={imageAlt}
             className="aspect-square rounded-lg items-center"
           />
         </div>
         <div className="w-full flex flex-col py-6 px-7 bg-[#2E303E] rounded ml-4">
           <div className="flex items-end mb-2">
-            <h2 className="text-3xl font-bold mr-3">
-              Software Engineer Intern
-            </h2>
-            <p className="text-[#E95278] font-bold">Adobe</p>
+            <h2 className="text-3xl font-bold mr-3">{positionName}</h2>
+            <p className="text-[#E95278] font-bold">{companyName}</p>
           </div>
-          <h2 className="text-1xl mr-3 mb-3">June 2023 - September 2023</h2>
+          <h2 className="text-1xl mr-3 mb-3">{dateOfExperience}</h2>
           <div className="flex">
-            <div className="border border-[#569CD6] py-1 px-4 rounded-full mr-4">
-              <h3 className="uppercase text-[#569CD6] text-xs font-bold text-center">
-                Javascript
-              </h3>
-            </div>
-            <div className="border border-[#569CD6] py-1 px-4 rounded-full mr-4">
-              <h3 className="uppercase text-[#569CD6] text-xs font-bold text-center">
-                Python
-              </h3>
-            </div>
-            <div className="border border-[#569CD6] py-1 px-4 rounded-full mr-4">
-              <h3 className="uppercase text-[#569CD6] text-xs font-bold text-center">
-                Node.js
-              </h3>
-            </div>
+            {programmingLanguages.map((language) => (
+              <div
+                key={language}
+                className="border border-[#569CD6] py-1 px-4 rounded-full mr-4"
+              >
+                <h3 className="uppercase text-[#569CD6] text-xs font-bold text-center">
+                  {language}
+                </h3>
+              </div>
+            ))}
           </div>
         </div>
       </div>
@@ -59,7 +64,7 @@ const Experience = ({ imageSrc, imageAlt }: ExperienceProps) => {
           <h3 className="mb-1">Read More</h3>
         </button>
         <button onClick={() => setIsOpen(!isOpen)} className="p-0 m-0">
-          <FaChevronDown />
+          {isOpen ? <FaChevronUp /> : <FaChevronDown />}
         </button>
       </div>
       <div
@@ -67,11 +72,11 @@ const Experience = ({ imageSrc, imageAlt }: ExperienceProps) => {
           isOpen ? "opacity-100 h-full" : "h-0"
         }`}
       >
-        <p className="text-[#E95278] font-bold mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non
-          risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
-          ultricies sed, dolor.
-        </p>
+        {description.map((paragraph) => (
+          <p key={paragraph} className="text-white font-bold mb-4">
+            - {paragraph}
+          </p>
+        ))}
       </div>
     </div>
   );
