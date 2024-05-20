d22ba2ff96e2341f82f5da067ba7065cc2c57510
Julius <juliuscecilia33@gmail.com>
2024-05-19 17:56:45 -0700

    created new pages

---
+++ src/app/blogs/adobetwo/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+export default function AdobeTwo() {
+  return (
+    <main className="flex flex-col">
+      <Navbar />
+      <div className={``}>
+        <h1>Adobe Summer 2023</h1>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+export default function Blogs() {
+  return (
+    <main className="flex flex-col">
+      <Navbar />
+      <div className={``}>
+        <h1>Blogs</h1>
+      </div>
+    </main>
+  );
+}
